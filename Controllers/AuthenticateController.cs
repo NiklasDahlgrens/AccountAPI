fce0d42c60857fa8de362d65a00a285d94b7caad
NiklasDahlgrens <96531368+NiklasDahlgrens@users.noreply.github.com>
2022-03-09 16:08:17 +0100

    Add files via upload

---
+++ Controllers/AuthenticateController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAPI.Controllers
+{
+
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly LoginDetailsContext _context;
+        public AuthenticateController(LoginDetailsContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Authenticate")]
+        [HttpGet]
+
+        public ActionResult<LoginDetails> GetUser(string Username, string Password)
+         {
+             //var User = await _context.LoginDetails.FindAsync(Username, Password);
+
+             var User =  _context.LoginDetails.Where(x => x.Username == Username && x.Password == Password).FirstOrDefault();
+
+             if (User == null)
+             {
+                 return NotFound();
+             }
+
+             return User;
+         }
+
+    }
+}
---
+++ Models/LoginDetails.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAPI.Models
+{
+    public class LoginDetails
+    {
+    
+        public int Id { get; set; }
+        public string Username { get; set; }
+        
+        public string Password { get; set; }
+        public int AccessLevel { get; set; }
+        public int UserId { get; set; }
+
+    
+
+    }
+}
